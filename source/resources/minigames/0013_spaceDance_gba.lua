26c39e23c5088ed49f7ad0f62bd56aca162d4ed8
RandomTestFive <randomtestfive@gmail.com>
2017-08-31 22:26:59 -0400

    Finish turn right

--- source/resources/minigames/0013_spaceDance_gba.lua
+++ source/resources/minigames/0013_spaceDance_gba.lua
@@ -14,7 +14,7 @@ function lminigame()
     dancers[i].anim = newAnimationGroup(img.sheet)
     dancers[i].anim:addAnimation("idle",0,0,fw,fh,2,50)
     dancers[i].anim:addFrame("idle",fw*2,0,fw,fh,10000000)
-    dancers[i].anim:addAnimation("turn",0,fh,fw,fh,3,150)
+    dancers[i].anim:addAnimation("turn",0,fh,fw,fh,3,225)
     dancers[i].anim:addFrame("turn",fw*3,fh,fw,fh,10000000)
     dancers[i].anim:addAnimation("right1",0,fh*2,fw,fh,2, 500)
     dancers[i].anim:addAnimation("right2",fw,fh*2,fw,fh,5, 75)
@@ -27,6 +27,25 @@ function lminigame()
 end
 
 function uminigame(dt)
+--player code
+  local hit = false
+  local barely = false
+  for _,s in pairs(currentHits) do
+    if s.name == "hit" then
+      hit = true
+      barely = s.bearly
+    end
+  end
+  
+  if input["pressA"] then
+    print("a")
+    if hit then
+      dancers[3].anim:setAnimation("right1")
+      print("right1")
+    end
+  end
+  
+  --other dancers (and player)
   for i = 0, 3 do
     if dancers[i].anim:getCurrentAnimation() == "right1" and dancers[i].anim:getCurrentFrame() == 1 then
       dancers[i].anim:setAnimation("right2")
@@ -35,27 +54,29 @@ function uminigame(dt)
       dancers[i].anim:setAnimation("idle")
     end
     for _,s in pairs(currentSounds) do
-      print(s.name)
       if s.name == "turn" then
         dancers[i].anim:setAnimation("turn")
-      elseif s.name == "right" and dancers[i].anim:getCurrentAnimation() == "right1" and dancers[i].anim:getCurrentFrame() == 1 then
-        dancers[i].anim:setAnimation("right2")
-      elseif s.name == "right" then
+      elseif s.name == "right" and not dancers[i].player then
         dancers[i].anim:setAnimation("right1")
+      elseif s.name == "right" and not hit and dancers[3].anim:getCurrentAnimation() == "turn" then
+        dancers[i].anim:setAnimation("idle")
       end
     end
     if dancers[i].anim:getCurrentAnimation() == "idle" and beat == 10 then
       dancers[i].anim:setFrame(0)  
     end
     dancers[i].anim:update(dt)
-   end
+  end
+  
 end
 
 function dminigame()
   setColorHex("0000d6")
   love.graphics.rectangle("fill",0,0,view.width,view.height)
   setColorHex("ffffff")
-  dancers[0].anim:draw(40,0,0,2,2)
+  for i = 3, 0, -1 do
+    dancers[i].anim:draw(150+120*i,60,0,3,3) --TODO: do some actual math
+  end
 end
 
 return {lminigame,uminigame,dminigame}
